6dfc4b28c9c9c27df459f339568e95c2137fbc87
Sampathbandla <112699029+Samdevops789@users.noreply.github.com>
2022-09-13 12:15:56 +0530

    Delete redis.sh

--- redis.sh
+++
@@ -1,8 +0,0 @@
-
-curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
- yum install redis-6.2.7 -y
-
- # Update Listen Ip
-
-   systemctl enable redis
-   systemctl start redis
