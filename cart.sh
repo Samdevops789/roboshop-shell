aeb2596dd8b8c393822db7db37213ef00a73b37a
Samdevops789 <Sampathbandla2@gmail.com>
2022-09-14 22:26:35 +0530

    updating ./learning-devops7/roboshop-shell

--- cart.sh
+++ cart.sh
@@ -1,6 +1,6 @@
 echo Setting Node Js repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
@@ -8,14 +8,15 @@ if [$? -eq 0]; then
 
 echo Installing Node Js
 yum install nodejs -y &>>/tmp/cart.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
     fi
+
 echo Adding a Application User
 useradd roboshop &>>/tmp/cart.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
@@ -24,7 +25,7 @@ if [$? -eq 0]; then
 echo Downloading the Application Content
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"&>>/tmp/cart.log
 cd /home/roboshop &>>/tmp/cart.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
@@ -32,7 +33,7 @@ if [$? -eq 0]; then
 
 echo Cleaning old application content
 rm -rf cart &>>/tmp/cart.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
@@ -41,7 +42,7 @@ echo Extract Application Archive
 unzip -o /tmp/cart.zip &>>/tmp/cart.log
 mv cart-main cart &>>/tmp/cart.log
 cd cart &>>/tmp/cart.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
@@ -49,7 +50,7 @@ if [$? -eq 0]; then
 
 echo Installing Node Js Dependicies
 npm install &>>/tmp/cart.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
@@ -58,9 +59,16 @@ if [$? -eq 0]; then
 echo Configuring the Cart SystemD Service
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
 systemctl daemon-reload &>>/tmp/cart.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    fi
+
+echo Starting and Enabling Application
 systemctl start cart &>>/tmp/cart.log
 systemctl enable cart &>>/tmp/cart.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
