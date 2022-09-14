10b38efdaa6f795dd54d4144169bb0d22c3c3890
Samdevops789 <Sampathbandla2@gmail.com>
2022-09-15 00:12:04 +0530

    updating ./learning-devops7/roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,18 +1,24 @@
-set -e
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
+source common.sh
+
+NODEJS
 
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 cd /home/roboshop
 unzip -o /tmp/catalogue.zip
+StatusCheck
+
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 npm install
+StatusCheck
+
 
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
+StatusCheck
+
 systemctl start catalogue
 systemctl enable catalogue
+StatusCheck
 
 
