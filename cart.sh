c76a75cdae138ee9cfc277c43664f33c970942f5
Samdevops789 <Sampathbandla2@gmail.com>
2022-09-14 20:34:48 +0530

    updating ./learning-devops7/roboshop-shell

--- cart.sh
+++ cart.sh
@@ -1,9 +1,10 @@
-set -e
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
 yum install nodejs -y &>>/tmp/cart.log
+echo $?
 
 useradd roboshop &>>/tmp/cart.log
+echo $?
 
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"&>>/tmp/cart.log
 cd /home/roboshop &>>/tmp/cart.log
@@ -11,8 +12,10 @@ unzip -o /tmp/cart.zip &>>/tmp/cart.log
 mv cart-main cart &>>/tmp/cart.log
 cd cart &>>/tmp/cart.log
 npm install &>>/tmp/cart.log
+echo $?
 
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
 systemctl daemon-reload &>>/tmp/cart.log
 systemctl start cart &>>/tmp/cart.log
 systemctl enable cart &>>/tmp/cart.log
+echo $?
