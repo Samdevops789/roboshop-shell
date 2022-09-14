53bb5f00bfc18a158c65a2d22b3df2861725ee6d
Samdevops789 <Sampathbandla2@gmail.com>
2022-09-15 01:00:42 +0530

    updating ./learning-devops7/roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -6,12 +6,5 @@ NODEJS
 
 
 
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-StatusCheck
-
-systemctl start catalogue
-systemctl enable catalogue
-StatusCheck
 
 
