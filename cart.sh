157edfca7f9a8b7f117a5e69750499132487ebdd
Samdevops789 <Sampathbandla2@gmail.com>
2022-09-14 21:27:19 +0530

    updating ./learning-devops7/roboshop-shell

--- cart.sh
+++ cart.sh
@@ -1,9 +1,18 @@
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
 yum install nodejs -y &>>/tmp/cart.log
-echo $?
+if [$? -eq 0]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    fi
 
 useradd roboshop &>>/tmp/cart.log
+if [$? -eq 0]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    fi
 
 
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"&>>/tmp/cart.log
@@ -12,10 +21,18 @@ unzip -o /tmp/cart.zip &>>/tmp/cart.log
 mv cart-main cart &>>/tmp/cart.log
 cd cart &>>/tmp/cart.log
 npm install &>>/tmp/cart.log
-echo $?
+if [$? -eq 0]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    fi
 
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
 systemctl daemon-reload &>>/tmp/cart.log
 systemctl start cart &>>/tmp/cart.log
 systemctl enable cart &>>/tmp/cart.log
-echo $?
+if [$? -eq 0]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    fi
