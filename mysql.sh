a724e1081ea440ab820d9ccc61d3d7643ea88514
Samdevops789 <Sampathbandla2@gmail.com>
2022-09-15 17:17:00 +0530

    updating ./learning-devops7/roboshop-shell

--- mysql.sh
+++ mysql.sh
@@ -1,14 +1,30 @@
-set -e
+source common.sh
 
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-yum install mysql-community-server -y
-systemctl enable mysqld
-systemctl start mysqld
+COMPONENT=mysql
+
+if [ -z "$MYSQL_PASSWORD" ]; then
+  echo -e "\e[33m env variable MYSQL_PASSWORD is missing\e[0m"
+  exit 1
+  fi
+
+echo setup YUM repo
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
+StatusCheck
+
+echo Install Mysql
+yum install mysql-community-server -y &>>${LOG}
+StatusCheck
+
+echo Start MYSQL
+systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
+StatusCheck
+
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 echo MYSQL_PASSWORD=$MYSQL_PASSWORD
-
 echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD}
+
+exit
 
 echo "uninstall plugin validate_password;" | mysql -uroot -p$MYSQL_PASSWORD
 #> uninstall plugin validate_password;
