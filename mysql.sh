332dfe0e6c62029f3bc2ae46677b47cd9e29a721
Samdevops789 <Sampathbandla2@gmail.com>
2022-09-15 17:46:12 +0530

    updating ./learning-devops7/roboshop-shell

--- mysql.sh
+++ mysql.sh
@@ -8,7 +8,7 @@ if [ -z "$MYSQL_PASSWORD" ]; then
   fi
 
 echo setup YUM repo
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo  &>>${LOG}
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
 StatusCheck
 
 echo Install Mysql
