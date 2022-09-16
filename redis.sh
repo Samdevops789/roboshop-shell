2d94dde92fd83129926f8afa03c2bcb242c0cddd
Samdevops789 <Sampathbandla2@gmail.com>
2022-09-16 15:38:59 +0530

    updating ./learning-devops7/roboshop-shell

--- redis.sh
+++ redis.sh
@@ -10,7 +10,9 @@ echo Install Redis
 yum install redis-6.2.7 -y &>>${LOG}
 StatusCheck
 
-# Update Listen IP
+echo Update REDIS Listen IP Address
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
+StatusCheck
 
 echo Start Redis Service
 systemctl enable redis &>>${LOG} && systemctl restart redis &>>${LOG}
