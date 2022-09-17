12a3ab4d430002d6b0334aea8d7547f7e97a642d
Samdevops789 <Sampathbandla2@gmail.com>
2022-09-17 17:10:53 +0530

    updating ./learning-devops7/roboshop-shell

--- dispatch.sh
+++ dispatch.sh
@@ -9,6 +9,7 @@ StatusCheck
 echo adding User
 useradd roboshop &>>${LOG}
 
+echo Dispatching
 curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip &>>${LOG}
 unzip -o /tmp/dispatch.zip &>>${LOG}
 mv dispatch-main dispatch &>>${LOG}
